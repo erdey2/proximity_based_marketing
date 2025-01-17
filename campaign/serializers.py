f6009752fa48be573a9a7492edc8234ecb61169b
Erdey <erdeysyoum@gmail.com>
2025-01-17 07:04:38 +0300

    empty check is added

--- campaign/serializers.py
+++ campaign/serializers.py
@@ -17,6 +17,9 @@ class AdvertisementsSerializer(serializers.ModelSerializer):
         """
         start_date = data.get('start_date')
         end_date = data.get('end_date')
+
+        if not start_date or not end_date:
+            raise serializers.ValidationError({"dates": "Both start_date and end_date must be provided." })
         if start_date >= end_date:
             raise serializers.ValidationError({"end_date": "End date must be greater than start date." })
         return data
