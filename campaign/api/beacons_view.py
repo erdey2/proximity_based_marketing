c8362c0d4727ba7258c6c0862109f5669e9e750a
Erdey <erdeysyoum@gmail.com>
2025-01-27 10:10:55 +0300

    api authentication added

--- campaign/api/beacons_view.py
+++ campaign/api/beacons_view.py
@@ -1,8 +1,11 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+
 from campaign.models import Beacons
 from campaign.serializers import BeaconsSerializer
 
@@ -54,7 +57,7 @@ def beacons_detail(request, uuid):
 @api_view(['GET'])
 def beacons_search(request):
     """
-    search for beacons based on query parameters
+    search for beacons based on query parameters (location_name)
     """
     location_name = request.query_params.get('location_name', None)
     # start with all beacons
@@ -74,6 +77,7 @@ def beacons_search(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def active_beacons(request):
     """
     Retrieve a count of active beacons or a list of active beacons.
@@ -96,3 +100,29 @@ def beacons_count(request):
     if not total_beacons:
         return Response({'message': 'No beacon found'}, status=404)
     return Response({"count": total_beacons, "message": f"Found {total_beacons} beacons."}, status=200)
+
+
+@api_view(['POST'])
+def beacons_info(request):
+    """
+    Receives data from mobile app
+    """
+    try:
+        uuid = request.data.get('uuid')  # Unique ID of the beacon
+        battery_status = request.data.get('battery_status')  # Battery percentage
+        rssi = request.data.get('rssi')  # Signal strength
+
+        if not uuid or battery_status is None or rssi is None:
+            return Response(
+                {"error": "Missing required fields (ssid, battery_status, rssi)"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        # Save data to the database
+        beacon_data = Beacons.objects.create(ssid=uuid, battery_status=battery_status, rssi=rssi)
+
+        # Serialize the data for response
+        serializer = BeaconsSerializer(beacon_data)
+
+        return Response({"message": "Beacon data received successfully", "data": serializer.data}, status=status.HTTP_201_CREATED )
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
