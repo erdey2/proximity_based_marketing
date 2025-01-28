30e7dbb54b6c458ffc0418905ea2501ca2d9de8c
Erdey <erdeysyoum@gmail.com>
2025-01-28 12:01:13 +0300

    filter advertisement by uuid

--- campaign/urls.py
+++ campaign/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from .api import advertisements_view, beacons_view, advertisements_log
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from campaign import scan_check
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('api/beacons/', beacons_view.beacons_list, name='beacons_list'),
-    path('api/beacons/<beacon_id:beacon_id>', beacons_view.beacons_detail, name='beacons_detail'),
+    path('api/beacons/<uuid:pk>', beacons_view.beacons_detail, name='beacons_detail'),
     path('api/beacons/search', beacons_view.beacons_search, name='beacons_search'),
     path('api/beacons/active', beacons_view.active_beacons, name='active_beacons'),
     path('api/beacons/count', beacons_view.beacons_count,  name='count_beacons'),
@@ -15,7 +14,7 @@ urlpatterns = [
     path('api/beacons/info', beacons_view.beacons_info, name='beacons_info'),
 
     path('api/advertisements/', advertisements_view.advertisements_list, name='advertisement_list'),
-    path('api/advertisements/<advertisement_id:advertisement_id>', advertisements_view.advertisements_detail, name='advertisement_detail'),
+    path('api/advertisements/<uuid:pk>', advertisements_view.advertisements_detail, name='advertisement_detail'),
     path('api/advertisements/active', advertisements_view.advertisements_active, name='advertisements_active'),
     path('api/advertisements/search', advertisements_view.advertisements_search, name='advertisements_search'),
 
@@ -27,8 +26,6 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    path('home/', scan_check.scan_beacons, name='scan_check'),
 
 
     #open api
