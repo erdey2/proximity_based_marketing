57cf2489573cc89d8e77cb1f9a35504f907d46ff
Erdey <erdeysyoum@gmail.com>
2025-02-14 11:01:49 +0300

    urls for messages added

--- campaign/urls.py
+++ campaign/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .api.v1 import advertisement_view, beacon_view, advertisement_log
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from .api.v1.beacon_messages import CreateMessages
+from .api.v1.beacon_messages import MessageCreate, MessageDetail
 
 
 urlpatterns = [
@@ -27,7 +27,8 @@ urlpatterns = [
     path('api/v1/logs/count', advertisement_log.LogsCount.as_view(), name='ads_count'),
 
     # beacon messages
-    path('api/v1/beacons/messages', CreateMessages.as_view(), name='beacon_messages'),
+    path('api/v1/beacons/messages', MessageCreate.as_view(), name='message_create'),
+    path('api/v1/beacons/messages/<uuid:pk>', MessageDetail.as_view(), name='message_detail'),
 
     # documentation
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
