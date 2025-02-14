9855d22f62438d9d82e60fadac589a597e1036d4
Erdey <erdeysyoum@gmail.com>
2025-02-14 10:59:06 +0300

    create list and detail views added

--- campaign/api/v1/beacon_messages.py
+++ campaign/api/v1/beacon_messages.py
@@ -1,9 +1,16 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import generics
 from campaign.models import BeaconMessage
 from campaign.serializers import BeaconMessageSerializer
 
-class CreateMessages(ListCreateAPIView):
+class MessageCreate(generics.ListCreateAPIView):
     """Create and List messages from beacons"""
     queryset = BeaconMessage.objects.all()
     serializer_class = BeaconMessageSerializer
+
+
+class MessageDetail(generics.RetrieveDestroyAPIView):
+    """"Get and delete messages"""
+    queryset = BeaconMessage.objects.all()
+    serializer_class = BeaconMessageSerializer
+
 
