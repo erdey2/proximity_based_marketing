e3542a3ab748dfa7310fb0ce34845194c3896e03
Erdey <erdeysyoum@gmail.com>
2025-03-05 16:37:42 +0300

    debug mode disabled

--- project/settings.py
+++ project/settings.py
@@ -28,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7vimodp6z3r5$e$7+%ab)*c-!xygdgwf3=8md3@a(o&o6e@9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-#ALLOWED_HOSTS = ['proximity-based-marketing.onrender.com', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['proximity-based-marketing.onrender.com', 'localhost', '127.0.0.1']
+#ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
