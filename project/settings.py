637ad270610b3324f8d6ccb04f52db9e812daf8c
Erdey <erdeysyoum@gmail.com>
2025-03-06 13:50:22 +0300

    beacon list serializer added

--- project/settings.py
+++ project/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7vimodp6z3r5$e$7+%ab)*c-!xygdgwf3=8md3@a(o&o6e@9b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['proximity-based-marketing.onrender.com', 'localhost', '127.0.0.1']
 #ALLOWED_HOSTS = ["*"]
