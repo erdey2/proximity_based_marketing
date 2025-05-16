d0ea271e945c183ac2fd7a3bdda930ba730e97c7
Erdey <erdeysyoum@gmail.com>
2025-05-16 13:15:40 +0300

    database settings updated

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,6 +4,17 @@ DEBUG = False
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proximity_market',
+        'USER': 'proximity_market_owner',
+        'PASSWORD': 'npg_V3ByzE9vpkZm',
+        'HOST': 'ep-aged-resonance-a5yura0n-pooler.us-east-2.aws.neon.tech',
+        'PORT': '5432',
+    }
+}
+
 # Secure settings
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
