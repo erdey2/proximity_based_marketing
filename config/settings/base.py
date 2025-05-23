d4d71d4993f24d5cb51ef4fe2680565593051e2c
Erdey <erdeysyoum@gmail.com>
2025-05-23 15:16:26 +0300

    logging info added

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,6 +13,7 @@ from pathlib import Path
 import os
 import environ
 from datetime import timedelta
+import logging
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
@@ -178,6 +179,20 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',  # You can override this in production.py
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- config/settings/production.py
+++ config/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 DEBUG = False
 
+LOGGING['root']['level'] = 'INFO'
+
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
